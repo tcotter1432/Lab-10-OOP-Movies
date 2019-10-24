1ffb382a28c53c7b32941d718cde127de92b8bb6
tcotter1432 <tcotter1432@gmail.com>
2019-10-24 16:34:41 -0400

    First Commit

---
+++ Lab10OOPMovies/Movie.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab10OOPMovies
+{
+    class Movie
+    {
+        private string name;
+        private string genre;
+        private bool liveAction;
+
+        #region Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Genre
+        {
+            get { return genre; }
+            set { genre = value; }
+        }
+        public bool LiveAction
+        {
+            get { return liveAction; }
+            set { liveAction = value; }
+        }
+        #endregion
+
+        public Movie(string _name, string _genre, bool _liveaction)
+        {
+            name = _name;
+            genre = _genre;
+            liveAction = _liveaction;
+        }
+
+        public static void PrintGenres(List<Movie> theList)
+        {
+            List<string> genres = new List<string>();
+            foreach(Movie movie in theList)
+            {
+                if (!genres.Contains(movie.Genre))
+                {
+                    genres.Add(movie.Genre);
+                    Console.WriteLine($"{genres.Count}. {movie.Genre}");
+                }
+            }
+        }
+        public static List<string> GetGenres(List<Movie> theList)
+        {
+            List<string> genres = new List<string>();
+            foreach (Movie movie in theList)
+            {
+                if (!genres.Contains(movie.Genre))
+                {
+                    genres.Add(movie.Genre);
+                }
+            }
+            return genres;
+        }
+
+    }
+}
---
+++ Lab10OOPMovies/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10OOPMovies
+{
+    class Program
+    {
+        public static List<Movie> theMovies = new List<Movie>();
+        static void Main(string[] args)
+        {
+            bool keepGoing = true;
+            PopulateList();
+
+            Console.WriteLine($"We have a list of {theMovies.Count} movies here....");
+            while (keepGoing)
+            {
+                Console.WriteLine("What would you like to know?\n 1. The Genres \n 2. What movies are of what genre.\n 3. Exit");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case ("1"):
+                        Movie.PrintGenres(theMovies);
+                    break;
+                    case ("2"):
+                        Console.WriteLine("What genre do you want list out? (Enter a number)");
+                        input = Console.ReadLine();
+                        HandleGenres(input);
+                    break;
+                    case ("3"):
+                        keepGoing = false;
+                        Console.WriteLine("Take care!");
+                    break;
+                    default:
+                        Console.WriteLine("Invalid input! Try again!");
+                        continue;
+                }
+            }
+        }
+
+        public static void HandleGenres(string input)
+        {
+            try
+            {
+                int.Parse(input);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+            int intInput = int.Parse(input)-1;
+            List<string> genres = new List<string>();
+            genres = Movie.GetGenres(theMovies);
+            if (intInput >= genres.Count || intInput < 0)
+            {
+                Console.WriteLine("Invalid Input!");
+                return;
+            }
+            Console.WriteLine($"You have selected {genres[intInput]}.");
+            foreach(Movie movie in theMovies)
+            {
+                if (genres[intInput] == movie.Genre)
+                {
+                    Console.WriteLine(movie.Name);
+                }
+            }
+        }
+
+        public static void PopulateList()
+        {
+            theMovies.Add(new Movie("Apollo 13", "Historical", true));
+            theMovies.Add(new Movie("Braveheart", "Medieval Action", true));
+            theMovies.Add(new Movie("The Fellowship of the Ring", "Fantasy", true));
+            theMovies.Add(new Movie("The Two Towers", "Fantasy", true));
+            theMovies.Add(new Movie("The Return of the King", "Fantasy", true));
+            theMovies.Add(new Movie("Big Hero 6", "Superhero", false));
+            theMovies.Add(new Movie("Top Gun", "Modern Action", true));
+            theMovies.Add(new Movie("Raiders of the Lost Arc", "Historical Fiction", true));
+            theMovies.Add(new Movie("Inglorious Bastards", "Tarantino Flick", true));
+            theMovies.Add(new Movie("Fight Club", "Tarantino Flick", true));
+        }
+    }
+}
